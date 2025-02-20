1b33eab133265fbb620cfd1c9809e32a48a70b54
happiness425 <90085910+happiness425@users.noreply.github.com>
2025-02-20 14:33:12 +0800

    Update data.py

--- data.py
+++ data.py
@@ -92,10 +92,10 @@ def download_S3DIS():
 def load_data_cls(partition):
     download_modelnet40()
   
-    DATA_DIR = '/content/gdrive/MyDrive/DGCNN/data/modelnet40-ply-hdf5-2048'
+    DATA_DIR = '/content/gdrive/MyDrive/DGCNN/data/modelnet40-ply-hdf5-2048/modelnet40_ply_hdf5_2048'
     all_data = []
     all_label = []
-    for h5_name in glob.glob(os.path.join(DATA_DIR, 'modelnet40_ply_hdf5_2048', '*%s*.h5'%partition)):
+    for h5_name in glob.glob(os.path.join(DATA_DIR, '*%s*.h5'%partition)):
         f = h5py.File(h5_name, 'r')
         data = f['data'][:].astype('float32')
         label = f['label'][:].astype('int64')
@@ -108,6 +108,7 @@ def load_data_cls(partition):
         raise ValueError("No data loaded. Check your dataset paths.")
     all_label = np.concatenate(all_label, axis=0)
     return all_data, all_label
+
 
 
 def load_data_partseg(partition):
